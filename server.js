402473d0d7b160686be659ddc8443c0bdae6ed15
ashwanikumar1414 <ashwani.kumar831991@gmail.com>
2026-05-30 16:35:51 +0530

    Added test docket

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello, Geeks!</h1><p>This is your simple Express server.</p>');
+    res.send('<h1>Hello, Geeks!</h1><p>This is your simple Express server.</p>');
 });
 
 if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(`Server is listening at http://localhost:${PORT}`);
-  });
+    app.listen(PORT, () => {
+        console.log(`Server is listening at http://localhost:${PORT}`);
+    });
 }
 
 module.exports = app;
